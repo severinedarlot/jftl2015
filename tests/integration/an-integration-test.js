4d8651f44aab7f94ba1b2c001fba93000613e6f0
Séverine Darlot <severine.darlot@gmail.com>
2015-04-03 20:07:48 +0000

    Remove ember-data, inactive live reload, add a first integration test

---
+++ tests/integration/an-integration-test.js
@@ -0,0 +1,20 @@
+import Ember from "ember";
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+var App;
+ 
+module('An Integration test', {
+  beforeEach: function() {
+    App = startApp();
+  },
+  afterEach: function() {
+    Ember.run(App, App.destroy);
+  }
+});
+
+test("The page has a title", function(assert) {
+  assert.expect(1);
+  visit('/').then(function() {
+    assert.equal(find('h2').text(), "Welcome to Ember.js", "Page contains the title");
+  });
+});
