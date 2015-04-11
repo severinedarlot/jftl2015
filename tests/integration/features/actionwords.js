9bbd95c1205b92076f2e81cd188316ddf6f9538f
Séverine Darlot <severine.darlot@gmail.com>
2015-04-11 20:41:37 +0000

    it2 first test

--- tests/integration/features/actionwords.js
+++ tests/integration/features/actionwords.js
@@ -4,35 +4,35 @@ var Actionwords = Ember.Object.extend({
   theCalculatorsPage: function (assert) {
     visit('/');
   },
-  addP1TitlesLessThan4Minutes: function (assert, p1) {
-    // TODO: Implement action: "Quand on ajoute " + String(p1) + " titres de moins de 4 minutes"
-    throw 'Not implemented';
-  },
-  theTTCPriceShouldBeP1TTC: function (assert, p1) {
-
-  },
   theTitlePageShouldBeP1: function (assert, p1) {
     andThen(function() {
       assert.equal(find('h2').text(), p1, "Page contains the title");
     });
   },
-  theLessThan4TitleIs: function (assert) {
-
-  },
   theFirstSliceIsP1: function (assert, p1) {
-
+    andThen(function() {
+      fillIn($('input.first'), p1);
+    });
   },
   theSecondSliceIsP1: function (assert, p1) {
-
+    andThen(function() {
+      fillIn($('input.second'), p1);
+    });
   },
   theThirdSliceIsP1: function (assert, p1) {
-
+    andThen(function() {
+      fillIn($('input.third'), p1);
+    });  
   },
   theFourthSliceIsP1: function (assert, p1) {
-
+    andThen(function() {
+      fillIn($('input.fourth'), p1);
+    });
   },
   theTitleTotalIsP1: function (assert, p1) {
-
+    andThen(function() {
+      assert.equal(find('#total-count').text(), p1, "The title total is computed");
+    });
   }
 });
 
--- tests/integration/features/project_test.js
+++ tests/integration/features/project_test.js
@@ -18,5 +18,14 @@ test('Calculator initialization (uid:bb889084-65f8-40e6-a976-b38e82660e53)', fun
   actionwords.theTitlePageShouldBeP1(assert, "Calcul de devis mastering");
 });
 
+test('Show the title total number (uid:d470e52e-3d0b-4afd-852e-b3b797a65b61)', function (assert) {
+  actionwords.theCalculatorsPage(assert);
+  actionwords.theFirstSliceIsP1(assert, "4");
+  actionwords.theSecondSliceIsP1(assert, "2");
+  actionwords.theThirdSliceIsP1(assert, "1");
+  actionwords.theFourthSliceIsP1(assert, "3");
+  actionwords.theTitleTotalIsP1(assert, "10");
+});
+
 
 
