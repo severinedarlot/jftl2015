55eebec65494da9a97f76940a16e59213fd0a170
Séverine Darlot <severine.darlot@gmail.com>
2015-04-11 21:21:15 +0000

    add price for one song

--- tests/integration/features/actionwords.js
+++ tests/integration/features/actionwords.js
@@ -4,34 +4,42 @@ var Actionwords = Ember.Object.extend({
   theCalculatorsPage: function (assert) {
     visit('/');
   },
+  theMasteringPriceShouldBePriceTTC: function (assert, price) {
+    andThen(function() {
+      assert.equal(find('#mastering-price').text(), price + ' €', 
+        "The mastering price is computed");
+    });
+  },
   theTitlePageShouldBeP1: function (assert, p1) {
     andThen(function() {
-      assert.equal(find('h2').text(), p1, "Page contains the title");
-    });
-  },
-  theFirstSliceIsP1: function (assert, p1) {
-    andThen(function() {
-      fillIn($('input.first'), p1);
-    });
-  },
-  theSecondSliceIsP1: function (assert, p1) {
-    andThen(function() {
-      fillIn($('input.second'), p1);
-    });
-  },
-  theThirdSliceIsP1: function (assert, p1) {
-    andThen(function() {
-      fillIn($('input.third'), p1);
-    });  
-  },
-  theFourthSliceIsP1: function (assert, p1) {
-    andThen(function() {
-      fillIn($('input.fourth'), p1);
+      assert.equal(find('h2').text(), p1, 
+        "Page contains the title");
     });
   },
   theTitleTotalIsP1: function (assert, p1) {
     andThen(function() {
-      assert.equal(find('#total-count').text(), p1, "The title total is computed");
+      assert.equal(find('#total-count').text(), p1, 
+        "The title total is computed");
+    });
+  },
+  theNthSliceIsNumber: function (assert, nth, number) {
+    var input;
+    switch(nth) {
+      case 'first':
+        input = 'input.first';
+        break;
+      case 'second':
+        input = 'input.second';
+        break;
+      case 'third':
+        input = 'input.third';
+        break;
+      case 'fourth':
+        input = 'input.fourth';
+        break;
+    }
+    andThen(function() {
+      fillIn(input, number);
     });
   }
 });
--- tests/integration/features/project_test.js
+++ tests/integration/features/project_test.js
@@ -20,12 +20,35 @@ test('Calculator initialization (uid:bb889084-65f8-40e6-a976-b38e82660e53)', fun
 
 test('Show the title total number (uid:d470e52e-3d0b-4afd-852e-b3b797a65b61)', function (assert) {
   actionwords.theCalculatorsPage(assert);
-  actionwords.theFirstSliceIsP1(assert, "4");
-  actionwords.theSecondSliceIsP1(assert, "2");
-  actionwords.theThirdSliceIsP1(assert, "1");
-  actionwords.theFourthSliceIsP1(assert, "3");
+  actionwords.theNthSliceIsNumber(assert, "first", "4");
+  actionwords.theNthSliceIsNumber(assert, "second", "2");
+  actionwords.theNthSliceIsNumber(assert, "third", "1");
+  actionwords.theNthSliceIsNumber(assert, "fourth", "3");
   actionwords.theTitleTotalIsP1(assert, "10");
 });
+
+function pricesForOneTitle (assert, nth, price) {
+  actionwords.theCalculatorsPage(assert);
+  actionwords.theNthSliceIsNumber(assert, String(nth), "1");
+  actionwords.theMasteringPriceShouldBePriceTTC(assert, String(price));
+}
+
+test('Prices for one title: Less than 4 minutes (uid:0270aee3-aed0-43a8-9196-90ee6348cc25)', function (assert) {
+  pricesForOneTitle.apply(this, [assert, 'first', 63]);
+});
+
+test('Prices for one title: Between 4 and 8 minutes (uid:251963de-5584-481a-9095-9381784e3f30)', function (assert) {
+  pricesForOneTitle.apply(this, [assert, 'second', 78.75]);
+});
+
+test('Prices for one title: Between 8 and 12 minutes (uid:b52179b3-aa18-451e-b1f2-09655331838c)', function (assert) {
+  pricesForOneTitle.apply(this, [assert, 'third', 94.5]);
+});
+
+test('Prices for one title: More than 12 minutes (uid:23fdff01-7636-494b-8c9a-9e086e1ae9d2)', function (assert) {
+  pricesForOneTitle.apply(this, [assert, 'fourth', 110.25]);
+});
+
 
 
 
