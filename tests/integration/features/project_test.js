2703d6f695fbd179c3b9ab0e38250e81bc7786be
Séverine Darlot <severine.darlot@gmail.com>
2015-04-11 21:57:42 +0000

    update test

--- tests/integration/features/project_test.js
+++ tests/integration/features/project_test.js
@@ -52,22 +52,58 @@ test('Prices for one title: More than 12 minutes (uid:23fdff01-7636-494b-8c9a-9e
 });
 
 
-function thePriceByTitleDecreasesAccordingTheTitleNumber (assert, title_count, total_price) {
+function forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber (assert, title_count, total_price) {
   actionwords.theCalculatorsPage(assert);
   actionwords.theNthSliceIsNumber(assert, "first", String(title_count));
   actionwords.theMasteringPriceShouldBePriceTTC(assert, String(total_price));
 }
 
-test('The price by title decreases according the title number: One (uid:8faaca20-002a-40dd-862b-b7c8d67c2989)', function (assert) {
-  thePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 1, 63]);
+test('For the first slice, the price by title decreases according the title number: One: 63 (uid:8faaca20-002a-40dd-862b-b7c8d67c2989)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 1, 63]);
 });
 
-test('The price by title decreases according the title number: Two is 62*2 (uid:c0c07640-5b4b-4ad3-b42c-e4dfd43be837)', function (assert) {
-  thePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 2, 124]);
+test('For the first slice, the price by title decreases according the title number: Two: 2 * (63-1) (uid:c0c07640-5b4b-4ad3-b42c-e4dfd43be837)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 2, 124]);
 });
 
-test('The price by title decreases according the title number: Six is 58*6 (uid:93c49689-c593-4398-8458-89c49258e31f)', function (assert) {
-  thePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 6, 348]);
+test('For the first slice, the price by title decreases according the title number: Six: 2 * (63 - 5) (uid:93c49689-c593-4398-8458-89c49258e31f)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 6, 348]);
+});
+
+test('For the first slice, the price by title decreases according the title number: Eleven: 11 * (63 - 10) (uid:3b694b9b-3b03-4933-a77d-76e85c227c5a)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 11, 583]);
+});
+
+test('For the first slice, the price by title decreases according the title number: Seven: 7 * (63 - 6) (uid:3e5ef3ce-ad86-4a5f-997e-46f8480d7eed)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 7, 399]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 12 * (63 - 11) (uid:6392658a-a01f-41e1-9b1c-e31e3a7f9539)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 12, 624]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 13 * (63 - 12) (uid:2fc65158-596c-44f6-9b95-0d47bcb2f1c4)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 13, 663]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 14 *(63 - 13) (uid:3807201a-dfdd-4a3b-ba1b-a05026a4c20d)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 14, 700]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 20 * (63 - 20) (uid:488032fd-02b3-4df5-987a-7deaa0962c03)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 20, 880]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 24 * (63 - 19) (uid:7bdc4921-a19b-4c95-9e69-e22af81c4bae)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 24, 960]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 25 * 40 (uid:88986b06-a8b1-4230-b3a7-d294a16a2fcc)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 25, 1000]);
+});
+
+test('For the first slice, the price by title decreases according the title number: 26 * 40 (uid:fba5408c-b0a1-497b-864a-c98b7fbec9d2)', function (assert) {
+  forTheFirstSliceThePriceByTitleDecreasesAccordingTheTitleNumber.apply(this, [assert, 26, 1040]);
 });
 
 
